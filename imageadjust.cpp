52ec905054badf7e95481afef84b7ab44a9e2496
Joel Andres Granados <joel.granados@gmail.com>
2010-07-30 14:13:30 +0200

    Reduce unnecessary comments and keep to standard.

--- imageadjust.cpp
+++ imageadjust.cpp
@@ -17,10 +17,7 @@ ia_calc_image_chess_points ( char **images, const Size boardSize,
     vector<Point2f> pointbuf; //temp buffer for object points
     Mat t_image; //temp image holder
 
-    /*
-     * We see if we can actually read the image,  the 0 means we read the image
-     * in gray scale.
-     */
+    /* Read the image in gray scale */
     t_image = imread(images[i], 0);
     if ( t_image.data == NULL )
     {
@@ -28,10 +25,7 @@ ia_calc_image_chess_points ( char **images, const Size boardSize,
       continue;
     }
 
-    /*
-     * We make note of the image size and tell the user if we found something
-     * fishy.
-     */
+    /* Keep the image size. tell the use if something fishy is going on. */
     if ( i > 0  //so we don't output unnecessary messages
          && ( t_image.size().height != generalSize->height
               || t_image.size().width != generalSize->width) )
@@ -44,27 +38,21 @@ ia_calc_image_chess_points ( char **images, const Size boardSize,
     else if ( i == 0 ) //We use the initial image size as general size.
       *generalSize = t_image.size();
 
-    /*
-     * We try to find the chessboard points in the image and put them in
-     * pointbuf.
-     */
+    /* Put chessboard points in pointbuff */
     if ( !findChessboardCorners(t_image, boardSize, pointbuf,
                                 CV_CALIB_CB_ADAPTIVE_THRESH) )
     {
-      //findChessboardCorners will return 0 if the corners where not found or
-      //they could not be organized.
+      /* Corners not found or not organized. */
       fprintf(stderr, "Could not find chess corners in image %s\n", images[i]);
       continue;
     }
 
-    // improve the found corners' coordinate accuracy
+    /* improve the found corners' coordinate accuracy */
     cornerSubPix( t_image, pointbuf, Size(11,11),
                   Size(-1,-1),
                   TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 30, 0.1 ));
 
-    /*
-     * We make sure we keep the image points and count this image as 'found'
-     */
+    /* Keep the found points */
     imagePoints->push_back(pointbuf);
   }
 
@@ -78,43 +66,28 @@ ia_calc_image_chess_points ( char **images, const Size boardSize,
   return true;
 }
 
-/**
- * Create the objectPoints vector.  It needs to be of the same size as the
- * imagePoints vector.  All the elements are the same because it is the same
- * object (the chessboard).
- *
- * @param boardSize the size of the board in number of corners
- * @param squareSize  the size of the squares in the board.  should be 1.
- * @param numElems The number of images that were found with
- * ia_calc_image_chess_points.
- */
+/* Puts object vector in objectPoints */
 bool
 ia_calc_object_chess_points ( const Size boardSize, const int squareSize,
                               const int numElems,
                               vector<vector<Point3f> > *objectPoints )
 {
-  // create one element of the objectPoints vector.
+  /* create one element of the objectPoints vector. */
   vector<Point3f> corners;
   for ( int i = 0 ; i < boardSize.height ; i++ )
     for ( int j = 0; j < boardSize.width ; j++ )
       corners.push_back( Point3f(double(j*squareSize),double(i*squareSize),0) );
 
-  // replicate that element size times
+  /* replicate that element size times */
   for ( int i = 0 ; i < numElems ; i++ )
     objectPoints->push_back(corners);
 
   return true;
 }
 
-/**
- * @param images A pointer to the image file names.
- * @param boardSize Contain the height and width of the object board.
- * @param camMat Holds the Camera matrix
- * @param disMat Holds the distortion coefficients
- * @param rvecs The list of rotational vectors.
- * @param tvecs The list of translational vectors.
- *
- * @return Returns true on success, false in any other case.
+/*
+ * Calculates camera matrix, camera distortion, rotational vector and
+ * translational vector
  */
 bool
 ia_calculate_all ( char **images, const Size boardSize, Mat& camMat,
@@ -345,7 +318,7 @@ main ( int argc, char** argv ) {
   Mat disMat;
   vector<Mat> rvecs, tvecs;
 
-  // Analyze the input
+  /* parse intput */
   if ( (input = ia_init_input(argc, argv)) == NULL )
     exit(0); //an error message has already been printed
 
