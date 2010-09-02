a26adc4f3f5d9f177a215331e9d34b926ce8508a
Joel Andres Granados <joel.granados@gmail.com>
2010-09-02 15:45:17 +0200

    Remove unneeded functions.
    * ia_core.cpp (ia_calc_image_chess_points, ia_calculate_all)
    (ia_calculate_extrinsics, ia_calculate_intrinsics)
    (ia_print_matrix, ia_print_matrix_vector): Remove functions
    * ia_core.h: Remove descriptions.

--- ia_core.h
+++ ia_core.h
@@ -21,20 +21,8 @@
 
 using namespace cv;
 
-bool ia_calc_image_chess_points ( char**, const Size,
-                                  vector<vector<Point2f> >*, Size* );
-
 bool ia_calc_object_chess_points ( const Size, const int, const int,
                                    vector<vector<Point3f> >* );
-
-bool ia_calculate_all ( char**, const Size, Mat*, Mat*,
-                        vector<Mat>*, vector<Mat>*, const float = 1);
-
-bool ia_calculate_intrinsics ( char**, const Size, Mat&, Mat&,
-                               const float = 1 );
-
-bool ia_calculate_extrinsics ( char**, const Mat*, const Mat*, const Size,
-                               vector<Mat>*, vector<Mat>*, const float = 1 );
 
 void ia_calculate_and_capture ( const Size, const int,  const char*,
                                 const int = 0, const float = 1,
