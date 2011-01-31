a2352cc293a77ae0a0300a969da4cace9d4523eb
Joel Andres Granados <joel.granados@gmail.com>
2011-01-31 16:41:39 +0100

    Add the python-C++ interface.
    * _ilac.cpp (ilac_get_image_id): New method that can be called from python.  It
    will have image file, width size and height size as its arguments.

---
+++ _ilac.cpp
@@ -0,0 +1,79 @@
+/*
+ * ILAC: Image labeling and Classifying
+ * Copyright (C) 2011 Joel Granados <joel.granados@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <Python.h>
+#include "ilacSquare.h"
+
+static PyObject*
+ilac_get_image_id ( PyObject *self, PyObject *args )
+{
+  char *image_file;
+  int size1, size2;
+  PyObject *list_image_id;
+  vector<unsigned short> image_id;
+
+  /* parse incoming arguments. */
+  if ( !PyArg_ParseTuple ( args, "sII", &image_file, &size1, &size2 ) )
+    return NULL;
+
+  /* Calculate the image id vector */
+  try
+  {
+    ILAC_ChessboardImage cb = ILAC_ChessboardImage ( image_file, size1, size2 );
+    image_id = cb.get_image_id ();
+  }
+  catch (std::exception& ilace)
+  {
+    PyErr_SetString ( PyExc_StandardError, ilace.what() );
+    return NULL;
+  }
+
+  /*Construct python list that will hold the image id*/
+  list_image_id = PyList_New ( image_id.size() );
+  if ( list_image_id == NULL )
+  {
+    PyErr_SetString ( PyExc_StandardError, "Error creating a new list." );
+    return NULL;
+  }
+
+  for ( int i = 0 ; i < image_id.size() ; i++ )
+    if ( PyList_SetItem ( list_image_id, i, Py_BuildValue("H", image_id[i]) )
+         == -1 )
+    {
+      PyErr_SetString ( PyExc_StandardError, "Error creating id list elem." );
+      return NULL;
+    }
+
+  return list_image_id;
+}
+
+static struct PyMethodDef ilac_methods [] =
+{
+  { "get_image_id",
+    (PyCFunction)ilac_get_image_id,
+    METH_VARARGS, "Analyzes the image file and returns an ide if a valid"
+      "chessboard was found." },
+  {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+init_ilac (void)
+{
+  (void) Py_InitModule ( "_ilac", ilac_methods );
+}
