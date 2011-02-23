f10126fed3afd996e10072530e5b015e90175c82
Joel Andres Granados <joel.granados@gmail.com>
2011-02-23 13:45:04 +0100

    Add ilac_process_classify_dir to the ilac module.
    * ilac.py (ilac_process_classify_dir): New method.  It classifies and modifies
    the images.

--- ilac.py
+++ ilac.py
@@ -72,9 +72,50 @@ def ilac_classify_dir( from_dir, to_dir, size1, size2 ):
     for root, dirs, files in os.walk(from_dir):
         for file in files:
             try:
-                ilac_classify_file(os.path.join(root,file), size1, size2, to_dir)
+                ilac_classify_file( os.path.join(root,file), \
+                                    size1, size2, to_dir )
             except Exception, err:
                 ilaclog.error( err )
+
+def ilac_process_classify_dir ( from_dir, to_dir, action, \
+                                size1, size2, distNorm, camMat, disMat ):
+    #Check that the two dirs exist.
+    for dir in [from_dir, to_dir]:
+        if not os.path.isdir(dir):
+            raise ILACDirException(dir)
+
+    for root, dirs, files in os.walk(from_dir):
+        for f in files:
+            try:
+                # process image to filename-new.
+                filename = os.path.join(root, f)
+                img_id = _ilac.process_image ( action, size1, size2, distNorm,
+                                               camMat, disMat,
+                                               filename, "%s-new"%filename )
+
+            except Exception, err:
+                ilaclog.error( err )
+
+            # Create id string that will be the dir name.
+            id_dir = ""
+            for dirpart in img_id:
+                id_dir = id_dir + str(dirpart)
+
+            # Make sure the "new" to_file_dir exists.
+            to_file_dir = os.path.join(to_dir, id_dir)
+            if ( not os.path.isdir( to_file_dir ) ):
+                os.mkdir( to_file_dir )
+
+            # We need the full to_file_name path
+            to_file_name = os.path.join(to_file_dir, f )
+
+            # shutil uses rename if on the same fielsystem.
+            shutil.move( from_file_name, to_file_name )
+
+            # tell the user about the move
+            ilaclog.debug("Moved %s to %s"%(from_file_name, to_file_name))
+
+
 
 class ILACException(Exception):
     def __init__(self):
