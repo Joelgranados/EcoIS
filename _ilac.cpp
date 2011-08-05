afba33a1c36d4033018f28dc04234d304d64e856
Joel Andres Granados <joel.granados@gmail.com>
2011-08-05 17:01:26 +0200

    Add python Chessboard Image skeleton.
    * _ilac.cpp (IlacCB): New struct that contains a ChessboardImage as a sole
    element.
    (IlacCB_init, IlacCB_version, IlacCB_members, IlacCB_methods)
    (IlacCBType, ilac_calc_process_image, init_ilac)
    (IlacCB_dealloc, IlacCB_new):  New functions and arrays that contain IlacCB's
    components.

--- _ilac.cpp
+++ _ilac.cpp
@@ -18,6 +18,7 @@
  */
 
 #include <Python.h>
+#include <structmember.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include "ilacSquare.h"
@@ -29,6 +30,135 @@
     return NULL; \
   }
 
+/*{{{ IlacCB Object*/
+typedef struct{
+  PyObject_HEAD /* ";" provided by macro*/
+  ILAC_ChessboardImage *cb;
+} IlacCB;
+
+static void
+IlacCB_dealloc ( IlacCB *self )
+{
+  delete self->cb;
+  self->ob_type->tp_free((PyObject*)self);
+}
+
+/* Creats (not instantiates) new object */
+static PyObject*
+IlacCB_new ( PyTypeObject *type, PyObject *args, PyObject *kwds )
+{
+  IlacCB *self;
+  self = (IlacCB *)type->tp_alloc(type, 0);
+  if ( self != NULL )
+    self->cb = NULL;
+  return (PyObject *)self;
+}
+
+/* When new object is instantiated, can be called multiple times. */
+static int
+IlacCB_init(IlacCB *self, PyObject *args, PyObject *kwds)
+{
+  char *image_file;
+  int size1, size2;
+  PyObject *camMat_pylist, *disMat_pylist;
+  Mat camMat_cvmat, disMat_cvmat;
+
+  /* We do nothing if cb has already been created */
+  if ( self->cb != NULL )
+    return 0;
+
+  /* parse incoming arguments. */
+  if ( !PyArg_ParseTuple ( args, "sIIOO", &image_file, &size1, &size2,
+                                        &camMat_pylist, &disMat_pylist ) )
+  {
+    PyErr_SetString ( PyExc_StandardError,
+        "Invalid parameters for IlacCB_init.");
+    return -1;
+  }
+
+  /* Lets create the disMat_cvmat var from the disMat_pylist */
+  disMat_cvmat = Mat::zeros( 1, 8, CV_64F );
+  for ( int i = 0 ; i < (int)PyList_Size(disMat_pylist) ; i++ )
+    disMat_cvmat.at<double>(0,i) = PyFloat_AsDouble (
+      PyList_GetItem (disMat_pylist, i) );
+
+  camMat_cvmat = Mat::zeros( 3, 3, CV_64F );
+  for ( int i = 0 ; i < 9 ; i++ )
+    camMat_cvmat.at<double>(floor(i/3), i%3) = PyFloat_AsDouble (
+        PyList_GetItem ( PyList_GetItem ( camMat_pylist, floor(i/3) ), i%3 ) );
+
+  /* Instantiate ILAC_ChessboardImage into an object */
+  try{
+    self->cb = new ILAC_ChessboardImage ( image_file, Size(size1,size2),
+                                          camMat_cvmat, disMat_cvmat);
+  }catch (std::exception& ilace)
+  {
+    PyErr_SetString ( PyExc_StandardError, ilace.what() );
+    return -1;
+  }
+
+  return 0;
+}
+
+static PyObject*
+IlacCB_version ( IlacCB *self )
+{
+  Py_RETURN_TRUE;
+}
+
+static PyMemberDef IlacCB_members[] = { {NULL} };
+
+static PyMethodDef IlacCB_methods[] = {
+  {"ver", (PyCFunction)IlacCB_version, METH_NOARGS,
+    "Return the version of IlacCB"},
+  {NULL}
+};
+
+static PyTypeObject IlacCBType = {
+  PyObject_HEAD_INIT(NULL)
+  0,                         /*ob_size*/
+  "_ilac.IlacCB",            /*tp_name*/
+  sizeof(IlacCB),            /*tp_basicsize*/
+  0,                         /*tp_itemsize*/
+  (destructor)IlacCB_dealloc,/*tp_dealloc*/
+  0,                         /*tp_print*/
+  0,                         /*tp_getattr*/
+  0,                         /*tp_setattr*/
+  0,                         /*tp_compare*/
+  0,                         /*tp_repr*/
+  0,                         /*tp_as_number*/
+  0,                         /*tp_as_sequence*/
+  0,                         /*tp_as_mapping*/
+  0,                         /*tp_hash */
+  0,                         /*tp_call*/
+  0,                         /*tp_str*/
+  0,                         /*tp_getattro*/
+  0,                         /*tp_setattro*/
+  0,                         /*tp_as_buffer*/
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+  "IlacCB objects",          /* tp_doc */
+  0,                         /* tp_traverse */
+  0,                         /* tp_clear */
+  0,                         /* tp_richcompare */
+  0,                         /* tp_weaklistoffset */
+  0,                         /* tp_iter */
+  0,                         /* tp_iternext */
+  IlacCB_methods,            /* tp_methods */
+  IlacCB_members,            /* tp_members */
+  0,                         /* tp_getset */
+  0,                         /* tp_base */
+  0,                         /* tp_dict */
+  0,                         /* tp_descr_get */
+  0,                         /* tp_descr_set */
+  0,                         /* tp_dictoffset */
+  (initproc)IlacCB_init,     /* tp_init */
+  0,                         /* tp_alloc */
+  IlacCB_new,                /* tp_new */
+};
+
+/*}}} IlacCB Object*/
+
+/*{{{ ilac Module Methods*/
 static PyObject*
 ilac_get_version ( PyObject *self, PyObject *args )
 {
@@ -145,7 +275,7 @@ static PyObject*
 ilac_calc_process_image ( PyObject *self, PyObject *args )
 {
   PyObject *camMat_pylist, *disMat_pylist;
-  int size1, size2;
+  int size1, size2, squareSize; /*squareSize is in pixels.*/
   Mat camMat_cvmat, disMat_cvmat;
   char *outfile, *infile;
   vector<unsigned short> image_id;
@@ -154,7 +284,7 @@ ilac_calc_process_image ( PyObject *self, PyObject *args )
 
   /* First pyobjcect is a list of lists (camMat)
    * second pyobject is a list of ints */
-  if ( !PyArg_ParseTuple ( args, "iiOOss", &size1, &size2,
+  if ( !PyArg_ParseTuple ( args, "iiiOOss", &size1, &size2, &squareSize,
                            &camMat_pylist, &disMat_pylist,
                            &infile, &outfile ) )
     ILAC_RETERR("Invalid parameters for ilac_calc_process_image.");
@@ -186,6 +316,9 @@ ilac_calc_process_image ( PyObject *self, PyObject *args )
   }catch(ILACExNoneRedSquare){
     PyErr_SetString ( PyExc_StandardError, "None red square found." );
     return NULL;
+  }catch(ILACExFileError){
+    PyErr_SetString ( PyExc_StandardError, "Unknown Image format" );
+    return NULL;
   }
 
   /*Construct python list that will hold the image id*/
@@ -199,7 +332,7 @@ ilac_calc_process_image ( PyObject *self, PyObject *args )
       ILAC_RETERR("Error creating id list elem.");
 
   /* call the image process method. */
-  cb.process_image ( outfile );
+  cb.process_image ( outfile, squareSize);
 
   return list_image_id;
 }
@@ -232,5 +365,19 @@ static struct PyMethodDef ilac_methods [] =
 PyMODINIT_FUNC
 init_ilac (void)
 {
-  (void) Py_InitModule ( "_ilac", ilac_methods );
+  //(void) Py_InitModule ( "_ilac", ilac_methods );
+  PyObject *m;
+
+  if ( PyType_Ready(&IlacCBType) < 0 )
+    return;
+
+  m = Py_InitModule3 ( "_ilac", ilac_methods,
+      "Module that Normalizes images based on a marker" );
+
+  if ( m == NULL )
+    return;
+
+  Py_INCREF ( &IlacCBType );
+  PyModule_AddObject ( m, "IlacCB", (PyObject *)&IlacCBType );
 }
+/*}}} ilac Module Methods*/
